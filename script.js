f22e6a303bddd28be2cfe99a823d39c60214e226
Sardor-Abduhamidov007 <sardorabduhamidov007@gmail.com>
2022-11-28 22:05:49 +0300

    my first game

---
+++ script.js
@@ -0,0 +1,41 @@
+function myFunction() {
+  let answer = prompt("O`yin o`ynaysizmi");
+  let answer1 = answer.toLowerCase();
+  if (answer1 == "ha") {
+    let yourName = prompt("Ismingizni kiriting");
+    console.log(yourName);
+    let yourName2 = yourName.charAt(0).toUpperCase() + yourName.toLowerCase().slice(1);
+    console.log(yourName2);
+    if (yourName2) {
+      let num = prompt(yourName2 + " 1 dan 7 gacha son kiriting");
+      let num2 = parseInt(num);
+      if (num2 > 0 && num2 < 8) {
+        let ran = Math.random() * 7 + 1;
+        let fix = Math.floor(ran);
+        console.log(num2, fix);
+        if (fix == num2) {
+          document.querySelector("h1").innerHTML =
+            "Tabriklaymiz siz to'g'ri topdingiz";
+        } else {
+          document.querySelector(
+            "h1"
+          ).innerHTML = `${yourName2} qayta urining bizning raqam ${fix} edi.`;
+        }
+      } else {
+        alert("Kiritilayotgan son 1 bilan 7 ni orasida bo`lishi kerak");
+      }
+    } else {
+      alert("Ism kiritish majburiy");
+    }
+  } else if (answer1 == "yoq") {
+    alert("Hayr sog` bo`ling");
+  } else {
+    alert("Siz faqatgina ha yoki yoq buyrug`ini kiritishingiz mumkin");
+  }
+}
+
+// let yourName = "sArDoR";
+// let yourName1 = yourName.toLowerCase();
+// let yourName2 = yourName1.charAt(0).toUpperCase();
+// let yourName3 = yourName2 + yourName1.slice(1);
+// console.log(yourName3);
